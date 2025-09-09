c3a594fd493ec58e9256b285b4165840ca0fb6e3
Tibberle2911 <tibber.le@gmail.com>
2025-09-09 23:56:48 +1000

    First commit

---
+++ WebApplication-SWE40006/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_SWE40006.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
