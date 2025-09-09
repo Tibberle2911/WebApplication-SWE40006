fd005c8babec7a5e67359f9b8cc3105548395674
Tibberle2911 <tibber.le@gmail.com>
2025-09-09 23:59:58 +1000

    commit

---
+++ A3-SWE40006/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace A3_SWE40006.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ A3-SWE40006/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace A3_SWE40006.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
